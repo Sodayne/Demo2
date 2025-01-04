316204fee396011aaa2020a3ec74433cbd2d7cd2
Sebastian Garcia <sebas@Sebass-Air.homenet.telecomitalia.it>
2025-01-04 16:43:49 +0100

    Aggiunto supporto per configurazione ambiente e personalizzazioni di avvio

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -6,9 +6,3 @@ figlet "Benvenuto su CodeAnywhere!" || echo "Benvenuto su CodeAnywhere!"
 # Altri comandi personalizzati
 echo "Configurazione completata. Buon lavoro!"
 
-# Imposta la scorciatoia Ctrl+Enter per eseguire il testo selezionato nel terminale
-code --install-extension vscode.keyboard-shortcut-extension
-
-# Imposta la scorciatoia di tastiera per l'azione 'workbench.action.terminal.runSelectedText'
-code --set-extensions "vscode.keyboard-shortcut-extension"
-code --execute "{"command": "workbench.action.terminal.runSelectedText", "key": "ctrl+enter"}"
