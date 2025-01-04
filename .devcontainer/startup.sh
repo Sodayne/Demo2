ca9ec191ee6ab31a41bb778ea631fb9acf7426f6
Sebastian Garcia <sebas@Sebass-Air.homenet.telecomitalia.it>
2025-01-04 18:06:23 +0100

    Aggiunto supporto per configurazione ambiente e personalizzazioni di avvio

---
+++ .devcontainer/startup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+    apt-get install -y figlet
+    apt-get install -y samtools
+
+# Esegui l'ASCII art dopo l'installazione dei pacchetti
+figlet -f slant 'Advanced Molecular Biology Labs'
