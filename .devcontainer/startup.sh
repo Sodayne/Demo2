14021f1cb7d5618ead651a4969383d28cd140cb2
Sebastian Garcia <sebas@Sebass-Air.homenet.telecomitalia.it>
2025-01-04 16:52:20 +0100

    Aggiunto supporto per configurazione ambiente e personalizzazioni di avvio

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -6,3 +6,5 @@ figlet "Benvenuto su CodeAnywhere!" || echo "Benvenuto su CodeAnywhere!"
 # Altri comandi personalizzati
 echo "Configurazione completata. Buon lavoro!"
 
+# Avvia direttamente il terminale
+exec /bin/bash
